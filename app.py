afc1eeebea2db5ed64508c76f9fe7358bb1ee243
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-20 14:05:30 +0200

    Removed sql connect

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+##import pyodbc
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
-import pyodbc
 
 ##server = 'homeautomation2.database.windows.net'
 ##database = 'homeautomation3'
@@ -9,24 +9,29 @@ import pyodbc
 ##password = 'Xiobh@nmart9'
 ##driver= '{ODBC Driver 17 for SQL Server}'
 
-cnxn = pyodbc.connect('Server=tcp:homeautomation2.database.windows.net,1433;Initial Catalog=homeautomation3;Persist Security Info=False;User ID=Grant;Password=Xiobh@nmart9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;')
-cursor = cnxn.cursor()
+##cnxn = pyodbc.connect('Server=tcp:homeautomation2.database.windows.net,1433;Initial Catalog=homeautomation3;Persist Security Info=False;User ID=Grant;Password=Xiobh@nmart9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;')
+##cursor = cnxn.cursor()
+
 
 @app.route('/humidity/real')
 def HumidityRealTime():
-   return {"Humidity":55}
+    return {"Humidity": 55}
+
 
 @app.route('/temperature/real')
 def TemperatureRealTime():
-   return {"Temperature":23}
+    return {"Temperature": 23}
+
 
 @app.route('/humidity/history')
 def HumidityHistory():
-   return [{"Temperature":23},{"Temperature":24}]
+    return [{"Temperature": 23}, {"Temperature": 24}]
+
 
 @app.route('/temperature/history')
 def TemperatureHistory():
-   return [{"Temperature":23},{"Temperature":24}]
+    return [{"Temperature": 23}, {"Temperature": 24}]
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
