dc375b8fa4d825ea467584c6cfcdabd55e72fda1
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-20 10:53:46 +0200

    Second push

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ app = Flask(__name__)
 
 
 @app.route('/humidity/real')
-def humidity():
+def HumidityRealTime():
    return {"Humidity":55}
 
 @app.route('/temperature/real')
-def temperature():
+def TemperatureRealTime():
    return {"Temperature":23}
+
+@app.route('/humidity/history')
+def HumidityHistory():
+   return [{"Date":"2022-05-20","Humidity":80},{"Date":"2022-05-21","Humidity":90}]
+
+@app.route('/temperature/history')
+def TemperatureHistory():
+   return [{"Date":"2022-05-20","Temperature":34},{"Date":"2022-05-21","Temperature":23}]
 
 if __name__ == '__main__':
    app.run()
