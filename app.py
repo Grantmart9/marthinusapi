e7de2eeec902245b35efb36ddfdb7dd9934ba14b
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-20 16:20:33 +0200

    new

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask
 app = Flask(__name__)
 import pyodbc
 
@@ -7,7 +7,7 @@ server = 'homeautomation2.database.windows.net'
 database = 'homeautomation3'
 username = 'Grant'
 password = 'Xiobh@nmart9'
-driver= '{ODBC Driver 13 for SQL Server}'
+driver= '{ODBC Driver 17 for SQL Server}'
 
 cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
