253ce0122fd354ff893352e7125b081c65880d37
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-21 19:05:55 +0200

    Hello

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
 from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
-import pyodbc
 
-server = 'homeautomation2.database.windows.net'
-database = 'homeautomation3'
-username = 'Grant'
-password = 'Xiobh@nmart9'
-driver= '{ODBC Driver 17 for SQL Server}'
+# Temperature Sensor real-time value
+@app.route("/temperature", methods=['GET'])
+def get_demo():
+    return "This is temperature"
 
-cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
+# Humidity Sensor real-time value
+@app.route("/humidity", methods=['GET'])
+def get_demo():
+    return "This is humidity"
 
-@app.route('/humidity/real')
-def HumidityRealTime():
-   return {"Humidity":55}
-
-@app.route('/temperature/real')
-def TemperatureRealTime():
-   return {"Temperature":23}
-
-@app.route('/humidity/history')
-def HumidityHistory():
-   return {"Humidity":23,"Humidity":24}
-
-@app.route('/temperature/history')
-def TemperatureHistory():
-   return {"Temperature":23,"Temperature":24}
 
 if __name__ == '__main__':
    app.run()
