e524dc6415a22e11d7c51a65aa2af224d261b304
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-21 19:36:11 +0200

    g

--- app.py
+++ app.py
@@ -3,23 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 # Temperature Sensor real-time value
-@app.route("/temperature/realtime", methods=['GET'])
+@app.route("/Sensor1", methods=['GET'])
 def TemperatureReal():
-    return "This is temperature real-time"
+    return {"RealTime":{"DateTime":"2022-05-10","Temperature":23,"Humidity":76},"Historical":[{"DateTime":"2022-05-10","Temperature":23,"Humidity":76},{"DateTime":"2022-05-10","Temperature":23,"Humidity":76}]}
 
-# Humidity Sensor real-time value
-@app.route("/humidity/realtime", methods=['GET'])
-def HumidityReal():
-    return "This is humidity real-time"
-# Temperature Sensor historical value
-@app.route("/temperature/realtime", methods=['GET'])
-def TemperatureHistory():
-    return "This is temperature History"
 
-# Humidity Sensor historical value
-@app.route("/humidity/realtime", methods=['GET'])
-def HumidityHistory():
-    return "This is humidity History"
 
 if __name__ == '__main__':
    app.run()
