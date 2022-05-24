e1dba5c2033f5d8df7b9ec4323d263f7fa7be792
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-24 21:08:50 +0200

    s

--- app.py
+++ app.py
@@ -8,55 +8,6 @@ cursor = cnxn.cursor()
 
 app = Flask(__name__)
 
-# Sensor 1
-@app.route("/Sensor1", methods=['GET'])
-def Sensor1():
-    return {
-        "realtime": {
-            "dateTime": "2022-05-21",
-            "temperature": 23.4,
-            "humidity": 76,
-            "temperature_set_point": 23.5,
-            "humidity_set_point": 76.8
-        },
-        "historical": [
-            {
-                "dateTime": "2022-05-21",
-                "temperature": 23.4,
-                "humidity": 76,
-                "temperature_set_point": 23.5,
-                "humidity_set_point": 76.8
-            },
-            {
-                "dateTime": "2022-05-21",
-                "temperature": 23.4,
-                "humidity": 76,
-                "temperature_set_point": 23.5,
-                "humidity_set_point": 76.8
-            },
-            {
-                "dateTime": "2022-05-21",
-                "temperature": 23.4,
-                "humidity": 76,
-                "temperature_set_point": 23.5,
-                "humidity_set_point": 76.8
-            }
-        ]
-    }
-# User Authentication
-@app.route("/Users", methods=['GET'])
-def Users():
-    return {
-        "Identification": {
-            "Name": "Grant",
-            "LastName": "Marthinus",
-            "email": "grantmarthinus@gmail.com",
-            "cell": "+27 72 528 1424",
-            "Token": "#nsjd2@1nndn",
-            "Autherized": True
-        }
-    }
-
 
 if __name__ == '__main__':
     app.run()
