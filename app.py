b33aec8ff2ca660ec41a6325f49c7e1afeabb976
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-25 20:19:10 +0200

    s

--- app.py
+++ app.py
@@ -1,72 +1,77 @@
 import pyodbc
 from datetime import datetime
-from flask import Flask
+from flask import Flask,jsonify
 
 server = 'tcp:homeautomation2.database.windows.net' 
 database = 'homeautomation3' 
 username = 'Grant' 
 password = 'Xiobh@nmart9' 
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Serve};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
 app = Flask(__name__)
 # Sensor 1
-@app.route("/Sensor1", methods=['GET'])
-def Sensor1():
+@app.route("/Sensor1/Humidity", methods=['GET'])
+def Sensor1Humidty():
     cursor.execute("SELECT * FROM Humidity")
-    row = cursor.fetchmany()
-    print(row);
-    cursor.execute("SELECT * FROM Temperature")
-    row = cursor.fetchmany()
-    print(row);
-    return {
-        "realtime": {
-            "dateTime": "2022-05-21",
-            "temperature": 23.4,
-            "humidity": 76,
-            "temperature_set_point": 23.5,
-            "humidity_set_point": 76.8
-        },
-        "historical": [
-            {
-                "dateTime": "2022-05-21",
-                "temperature": 23.4,
-                "humidity": 76,
-                "temperature_set_point": 23.5,
-                "humidity_set_point": 76.8
-            },
-            {
-                "dateTime": "2022-05-21",
-                "temperature": 23.4,
-                "humidity": 76,
-                "temperature_set_point": 23.5,
-                "humidity_set_point": 76.8
-            },
-            {
-                "dateTime": "2022-05-21",
-                "temperature": 23.4,
-                "humidity": 76,
-                "temperature_set_point": 23.5,
-                "humidity_set_point": 76.8
-            }
-        ]
-    }
+    result = cursor.fetchmany(1)
 
-# User Authentication
+    ## create list ##
+    data = []
+    ## Loop through cols and historySize ##
+    for row in result:
+        ## Create dictionary for list ##
+        DataDictionary = {"Index": 1,"Date":(row[1]) ,"Humidity":(row[2])}
+
+        ## Append dictionary to list ##
+        data.append(DataDictionary)
+
+    ## print data to console ##
+    print(DataDictionary)
+ 
+    ## Return list and convert to json ##
+    return (jsonify(DataDictionary))
+
+@app.route("/Sensor1/Temperature", methods=['GET'])
+def Sensor1Temperature():
+    cursor.execute("SELECT * FROM Temperature")
+    result = cursor.fetchmany(1)
+
+    ## create list ##
+    data = []
+    ## Loop through cols and historySize ##
+    for row in result:
+        ## Create dictionary for list ##
+        DataDictionary = {"Index": 1,"Date":(row[1]) ,"Humidity":(row[2])}
+
+        ## Append dictionary to list ##
+        data.append(DataDictionary)
+
+    ## print data to console ##
+    print(DataDictionary)
+ 
+    ## Return list and convert to json ##
+    return (jsonify(DataDictionary))
+
 @app.route("/Users", methods=['GET'])
-def Users():
+def Sensor1Users():
     cursor.execute("SELECT * FROM Users")
-    row = cursor.fetchmany()
-    print(row);
-    return {
-        "Identification": {
-            "Name": "Grant",
-            "LastName": "Marthinus",
-            "email": "grantmarthinus@gmail.com",
-            "cell": "+27 72 528 1424",
-            "Token": "#nsjd2@1nndn",
-            "Autherized": "1"
-        }
-    }
+    result = cursor.fetchmany(1)
+
+    ## create list ##
+    data = []
+    ## Loop through cols and historySize ##
+    for row in result:
+        ## Create dictionary for list ##
+        DataDictionary = {"Index": 1,"Username":(row[1]) ,"Password":(row[2])}
+
+        ## Append dictionary to list ##
+        data.append(DataDictionary)
+
+    ## print data to console ##
+    print(DataDictionary)
+ 
+    ## Return list and convert to json ##
+    return (jsonify(DataDictionary))
 if __name__ == '__main__':
     app.run()
