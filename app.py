2afd0b71593074212d07ecba004667e91caf409e
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-25 13:48:34 +0200

    s

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pyodbc
 from datetime import datetime
 from flask import Flask
 
-cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:homeautomation2.database.windows.net,1433;Database=homeautomation3;Uid=Grant;Pwd={Xiobh@nmart9};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+##cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:homeautomation2.database.windows.net,1433;Database=homeautomation3;Uid=Grant;Pwd={Xiobh@nmart9};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 
 ##cursor = cnxn.cursor()
 
@@ -42,7 +42,7 @@ def Sensor1():
             }
         ]
     }
-    
+
 # User Authentication
 @app.route("/Users", methods=['GET'])
 def Users():
