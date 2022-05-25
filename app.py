3c9685e4cb5da1e92fad97c980a974101e53cebb
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-25 14:09:26 +0200

    s

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from flask import Flask
 
 ##cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:homeautomation2.database.windows.net,1433;Database=homeautomation3;Uid=Grant;Pwd={Xiobh@nmart9};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-
 ##cursor = cnxn.cursor()
 
 app = Flask(__name__)
