cf88f0f3e0f6f88cb4f60fc85cd807fd0c8fc9eb
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-05-27 20:14:25 +0200

    d

--- app.py
+++ app.py
@@ -38,8 +38,9 @@ jwt = JWT(app, authenticate, identity)
 ##############                These endpoints are protected  ######################################################
 ###################################################################################################################
 
-###########  Room Temperature & Humidity
-@app.route('/RoomTemperatureHumidity')
+##############                   Room Temperature & Humidity  #####################################################
+###################################################################################################################
+@app.route('/room')
 @jwt_required()
 def protected():
     return ({
@@ -124,6 +125,50 @@ def protected():
         "Date": "2022-05-27 18:50:12:342"
     }
 })
+
+##############                   Irrigation           #############################################################
+###################################################################################################################
+@app.route('/irrigation')
+@jwt_required()
+def irrigation():
+    return ({
+        "current_flow": "2",
+        "soil_humidity":"33",
+        "accumulated_flow_today":"334",
+        "total_water_day": [],
+        "out_door_temperature": "22",
+        "out_door_humidity": "85",
+        "irrigation_start_times":[],
+        "Date": "2022-05-27 18:50:12:342"
+        })
+
+##############                   Fertigation           #############################################################
+###################################################################################################################
+@app.route('/fertigation')
+@jwt_required()
+def fertigation():
+    return ({
+        "Mix1Setpoint": "2.2",
+        "Mix2Setpoint":"3.3",
+        "Mix3Setpoint":"5.4",
+        "Mix4Setpoint":"6.1",
+        "Mix5Setpoint":"6.4",
+        "Mix6Setpoint":"3.0",
+        "pH_Setpoint":"4.5",
+        })
+
+##############                   Energy          #############################################################
+###################################################################################################################
+@app.route('/energy')
+@jwt_required()
+def energy():
+    return ({
+        "inverter_power_output": "12",
+        "battery_time_left":"30",
+        "batter_percentage":"99.78",
+        "inverter_solar_charge":"300",
+        "inverter_charge_history":[],
+        })
 
 if __name__ == '__main__':
     app.run()
