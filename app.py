8b380f0910a1bf074171c40876b7cd5ac301b182
Grant Marthinus <grant.marthinus@lodicon.co.za>
2022-06-05 21:44:51 +0200

    d

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
 from flask_cors import CORS
+import sqlite3
+
+#### Connect to database ####
+con = sqlite3.connect("users.db")
+
 
 class User(object):
     def __init__(self, id, username, password):
@@ -12,28 +17,32 @@ class User(object):
     def __str__(self):
         return "User(id='%s')" % self.id
 
+
 users = [
-    User(1, 'Grant', 'Xiobh@nmart9'),
-    User(2, 'Xio', '9594'),
+    User(1, "Grant", "Xiobh@nmart9"),
+    User(2, "Xio", "1234"),
+    User(3, "Bion", "1234"),
 ]
 
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
+
 
 def authenticate(username, password):
     user = username_table.get(username, None)
     if user and safe_str_cmp(user.password, password):
         return user
 
+
 def identity(payload):
-    user_id = payload['identity']
+    user_id = payload["identity"]
     return userid_table.get(user_id, None)
+
 
 app = Flask(__name__)
 CORS(app)
 app.debug = True
-app.config['SECRET_KEY'] = 'super-secret'
-
+app.config["SECRET_KEY"] = "super-secret"
 jwt = JWT(app, authenticate, identity)
 
 ###################################################################################################################
@@ -42,229 +51,228 @@ jwt = JWT(app, authenticate, identity)
 
 ##############                   Room Temperature & Humidity  #####################################################
 ###################################################################################################################
-@app.route('/room')
+@app.route("/room")
 @jwt_required()
 def room():
-    return ({
-    "room_1": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_2": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_3": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_4": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_5": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_6": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_7": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_8": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_9": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_10": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
+    return {
+        "room_1": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_2": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_3": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_4": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_5": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_6": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_7": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_8": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_9": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "room_10": {
+            "Temperature": "23",
+            "Humidity": "88",
+            "O2": "2",
+            "TemperatureSetpoint": "22",
+            "HumiditySetpoint": "85",
+            "Date": "2022-05-27 18:50:12:342",
+        },
     }
-})
-##############                   Room Temperature & Humidity  #####################################################
+
+
+##############                   Dashboard                    #####################################################
 ###################################################################################################################
-@app.route('/dashboard')
+@app.route("/dashboard")
 @jwt_required()
 def dashboard():
-    return ({
-    "room_1": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_2": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_3": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_4": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_5": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_6": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_7": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_8": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_9": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
-    },
-    "room_10": {
-        "Temperature": "23",
-        "Humidity": "88",
-        "O2":"2",
-        "TemperatureSetpoint": "22",
-        "HumiditySetpoint": "85",
-        "Date": "2022-05-27 18:50:12:342"
+    return {
+        "irrigation": {
+            "current_flow": "2",
+            "soil_humidity": "33",
+            "accumulated_flow_today": "334",
+            "total_water_day": [],
+            "out_door_temperature": "22",
+            "out_door_humidity": "85",
+            "irrigation_start_times": [],
+            "Date": "2022-05-27 18:50:12:342",
+        },
+        "fertigation": {
+            "Mix1Setpoint": "2.2",
+            "Mix2Setpoint": "3.3",
+            "Mix3Setpoint": "5.4",
+            "Mix4Setpoint": "6.1",
+            "Mix5Setpoint": "6.4",
+            "Mix6Setpoint": "3.0",
+            "pH_Setpoint": "4.5",
+        },
+        "energy": {
+            "inverter_power_output": "12",
+            "battery_time_left": "30",
+            "batter_percentage": "99.78",
+            "inverter_solar_charge": "300",
+            "inverter_charge_history": [],
+        },
+        "weather": {
+            "Icon": "13",
+            "IconPhrase": "Mostly cloudy w/ showers",
+            "HasPrecipitation": "true",
+            "PrecipitationType": "Rain",
+            "PrecipitationIntensity": "Light",
+            "ShortPhrase": "Variable clouds with a shower",
+            "LongPhrase": "Variable clouds with a shower in the area",
+            "PrecipitationProbability": "44",
+            "ThunderstormProbability": "9",
+            "RainProbability": "44",
+            "Wind": {
+                "Speed": {"Value": "11.1", "Unit": "km/h", "UnitType": "7"},
+                "Direction": {"Degrees": "219", "Localized": "SW", "English": "SW"},
+            },
+            "WindGust": {
+                "Speed": {"Value": "29.6", "Unit": "km/h", "UnitType": "7"},
+                "Direction": {"Degrees": "242", "Localized": "WSW", "English": "WSW"},
+            },
+            "Rain": {"Value": "0.6", "Unit": "mm", "UnitType": "3"},
+            "Evapotranspiration": {"Value": "0.2", "Unit": "mm", "UnitType": "3"},
+            "SolarIrradiance": {"Value": "514.2", "Unit": "W/m²", "UnitType": "33"},
+        },
     }
-})
+
 
 ##############                   Irrigation           #############################################################
 ###################################################################################################################
-@app.route('/irrigation')
+@app.route("/irrigation")
 @jwt_required()
 def irrigation():
-    return ({
+    return {
         "current_flow": "2",
-        "soil_humidity":"33",
-        "accumulated_flow_today":"334",
+        "soil_humidity": "33",
+        "accumulated_flow_today": "334",
         "total_water_day": [],
         "out_door_temperature": "22",
         "out_door_humidity": "85",
-        "irrigation_start_times":[],
-        "Date": "2022-05-27 18:50:12:342"
-        })
+        "irrigation_start_times": [],
+        "Date": "2022-05-27 18:50:12:342",
+    }
+
 
 ##############                   Fertigation           ############################################################
 ###################################################################################################################
-@app.route('/fertigation')
+@app.route("/fertigation")
 @jwt_required()
 def fertigation():
-    return ({
+    return {
         "Mix1Setpoint": "2.2",
-        "Mix2Setpoint":"3.3",
-        "Mix3Setpoint":"5.4",
-        "Mix4Setpoint":"6.1",
-        "Mix5Setpoint":"6.4",
-        "Mix6Setpoint":"3.0",
-        "pH_Setpoint":"4.5",
-        })
+        "Mix2Setpoint": "3.3",
+        "Mix3Setpoint": "5.4",
+        "Mix4Setpoint": "6.1",
+        "Mix5Setpoint": "6.4",
+        "Mix6Setpoint": "3.0",
+        "pH_Setpoint": "4.5",
+    }
+
 
 ##############                   Energy          ##################################################################
 ###################################################################################################################
-@app.route('/energy')
+@app.route("/energy")
 @jwt_required()
 def energy():
-    return ({
+    return {
         "inverter_power_output": "12",
-        "battery_time_left":"30",
-        "batter_percentage":"99.78",
-        "inverter_solar_charge":"300",
-        "inverter_charge_history":[],
-        })
+        "battery_time_left": "30",
+        "batter_percentage": "99.78",
+        "inverter_solar_charge": "300",
+        "inverter_charge_history": [],
+    }
+
+##############                   Security          ################################################################
+###################################################################################################################
+
+@app.route("/security")
+@jwt_required()
+def security():
+    return {
+        "zone_1": {
+            "device_type": "PIR sensor",
+            "alarm_status": "Active",
+            "alarm": "OFF",
+        },
+        "zone_2": {
+            "device_type": "Magnet switch",
+            "alarm_status": "Active",
+            "alarm": "OFF",
+        },
+        "zone_3": {
+            "device_type": "IR Camera",
+            "alarm_status": "Active",
+            "alarm": "OFF",
+        }
+    }
+
 
 ##############                   Weather          #################################################################
 ###################################################################################################################
-@app.route('/weather')
+@app.route("/weather")
 @jwt_required()
 def weather():
-    return ({
+    return {
         "Icon": "13",
         "IconPhrase": "Mostly cloudy w/ showers",
         "HasPrecipitation": "true",
@@ -276,45 +284,18 @@ def weather():
         "ThunderstormProbability": "9",
         "RainProbability": "44",
         "Wind": {
-          "Speed": {
-            "Value": "11.1",
-            "Unit": "km/h",
-            "UnitType": "7"
-          },
-          "Direction": {
-            "Degrees": "219",
-            "Localized": "SW",
-            "English": "SW"
-          }
+            "Speed": {"Value": "11.1", "Unit": "km/h", "UnitType": "7"},
+            "Direction": {"Degrees": "219", "Localized": "SW", "English": "SW"},
         },
         "WindGust": {
-          "Speed": {
-            "Value": "29.6",
-            "Unit": "km/h",
-            "UnitType": "7"
-          },
-          "Direction": {
-            "Degrees": "242",
-            "Localized": "WSW",
-            "English": "WSW"
-          }
+            "Speed": {"Value": "29.6", "Unit": "km/h", "UnitType": "7"},
+            "Direction": {"Degrees": "242", "Localized": "WSW", "English": "WSW"},
         },
-        "Rain": {
-          "Value": "0.6",
-          "Unit": "mm",
-          "UnitType": "3"
-        },
-        "Evapotranspiration": {
-          "Value": "0.2",
-          "Unit": "mm",
-          "UnitType": "3"
-        },
-        "SolarIrradiance": {
-          "Value": "514.2",
-          "Unit": "W/m²",
-          "UnitType": "33"
-        }
-      })
+        "Rain": {"Value": "0.6", "Unit": "mm", "UnitType": "3"},
+        "Evapotranspiration": {"Value": "0.2", "Unit": "mm", "UnitType": "3"},
+        "SolarIrradiance": {"Value": "514.2", "Unit": "W/m²", "UnitType": "33"},
+    }
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
